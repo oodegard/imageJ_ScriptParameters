c4f50ef1c10d06f780e8c58239ea34b59ebdfba6
oodegard <oodegard@gmail.com>
2021-06-10 12:28:56 +0200

    Update call_hello_imageJ_module.py

--- 04_hello_python_module/call_hello_imageJ_module.py
+++ 04_hello_python_module/call_hello_imageJ_module.py
@@ -3,7 +3,7 @@ import hello_imageJ_module as him
 # List all possible functions
 print dir(him)
 
-# you can run your function like this
+# You can run your function like this
 him.helloImageJ("normic")
 him.helloImageJ_upper("normic")
 
