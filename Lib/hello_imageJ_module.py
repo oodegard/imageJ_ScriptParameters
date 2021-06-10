8b31974cdbbaaffff1fc52938b5d28b2475ee7ee
oodegard <oodegard@gmail.com>
2021-06-10 15:38:31 +0200

    Create hello_imageJ_module.py

---
+++ Lib/hello_imageJ_module.py
@@ -0,0 +1,7 @@
+# based on https://imagej.net/scripting/jython/#self-written-jython-modules-for-imagej
+
+def helloImageJ(name):
+	print "hello", name
+
+def helloImageJ_upper(name):
+	print "hello", (name.upper())
